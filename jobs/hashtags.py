a3a1943493ddbb5ab2fc172858adc81f4515493d
Ari Pratama Zhorifiandi <arizho16@gmail.com>
2019-11-27 20:54:31 +0700

    fresh or yeay

--- jobs/hashtags.py
+++ jobs/hashtags.py
@@ -2,6 +2,8 @@ def generate_hashtags(version):
     source = 'data/hashtags_combination.txt'
     if version == "v2":
         source = 'data/hashtags_combination_v2.txt'
+    elif version == "foy":
+        source = 'data/foy.txt'
     lines = [line.rstrip('\n') for line in open(source)]
     import random
     random.shuffle(lines)
